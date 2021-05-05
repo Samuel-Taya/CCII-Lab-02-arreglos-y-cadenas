83d09cc4443e171da5570d6c448be116878b92c1
Samuel-Taya <42224854+Samuel-Taya@users.noreply.github.com>
2021-05-05 11:56:54 -0500

    Update Problem4.cpp

--- Problem4.cpp
+++ Problem4.cpp
@@ -6,14 +6,14 @@ using namespace std;
 int tamanio( char c[])
 {
     int n=0;
-    while (c[n] != '/0')
+    while (c[n] != 0)
         n++;
     return n;
 }
 
 int main()
 {
-    char cadena[28]="This is a example of string";
+    char cadena[]="This is a example of string";
 
     cout << "The size of the string is: " << tamanio(cadena);
 
